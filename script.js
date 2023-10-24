1b6e00683a3d9dafbc53d37b7fb55ba083f45003
Niloy <niloyman01@gmail.com>
2023-10-24 19:00:16 +0600

    Color Picker

--- script.js
+++ script.js
@@ -56,3 +56,30 @@ function getMousePos(canvas, e) {
     y: e.clientY - rect.top,
   };
 }
+
+// Color Picker logic
+const colorPicker = document.getElementById("color-picker");
+colorPicker.addEventListener("mousedown", (e) => {
+  isDraggingColorPicker = true;
+
+  // Calculate the initial offset from the mouse to the color picker
+  const offsetX = e.clientX - colorPicker.getBoundingClientRect().left;
+  const offsetY = e.clientY - colorPicker.getBoundingClientRect().top;
+
+  document.addEventListener("mousemove", moveColorPicker);
+
+  document.addEventListener("mouseup", () => {
+    isDraggingColorPicker = false;
+    document.removeEventListener("mousemove", moveColorPicker);
+  });
+
+  function moveColorPicker(e) {
+    if (isDraggingColorPicker) {
+      const x = e.clientX - offsetX;
+      const y = e.clientY - offsetY;
+
+      colorPicker.style.left = x + "px";
+      colorPicker.style.top = y + "px";
+    }
+  }
+});
